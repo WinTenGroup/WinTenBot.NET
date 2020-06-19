956c961128215b1b5dcaea4f10e7708a5854cfbf
Azhe Kun <azhe403@gmail.com>
2020-06-19 23:53:28 +0700

    add cloud firestore example

---
+++ src/WinTenBot/WinTenBot/Tools/CloudFirestore.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+using Serilog;
+using WinTenBot.Common;
+using WinTenBot.IO;
+using WinTenBot.Model;
+
+namespace WinTenBot.Tools
+{
+    public static class CloudFirestore
+    {
+        private const string ProjectId = "wintenbot";
+        private static FirestoreDb Db { get; set; }
+
+        private static void MakeClient()
+        {
+            var credPath = BotSettings.GoogleCloudCredentialsPath.SanitizeSlash();
+            Log.Information($"Create Firestore client, cred {credPath}");
+            var clientBuilder = new FirestoreDbBuilder()
+            {
+                CredentialsPath = credPath,
+                ProjectId = ProjectId
+            };
+
+            var client = clientBuilder.Build();
+            Db = client;
+        }
+
+        public static void Create(string path, object data)
+        {
+            if (Db == null) MakeClient();
+            
+            // var a = FirestoreDb.Create(projectId);
+            Log.Information($"Adding data to {path}");
+            Log.Debug($"Data: {data.ToJson(true)}");
+
+            var collection = Db.Collection(path);
+            collection.AddAsync(data);
+        }
+
+        public static async Task ListDocument()
+        {
+            var collectionReference = Db.Collection("");
+            var docs = await collectionReference.GetSnapshotAsync();
+
+            foreach (var VARIABLE in docs)
+            {
+                Logger.LogInfo($"{VARIABLE.ToJson(true)}");
+            }
+        }
+    }
+}
