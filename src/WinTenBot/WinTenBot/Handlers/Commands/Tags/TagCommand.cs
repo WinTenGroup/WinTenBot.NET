8fdeaa5fc81e0cd00abe4ae1284191f41037ae2a
Azhe Kun <azhe403@gmail.com>
2020-06-27 18:48:59 +0700

    fix get raw button on creating tag with button

--- src/WinTenBot/WinTenBot/Handlers/Commands/Tags/TagCommand.cs
+++ src/WinTenBot/WinTenBot/Handlers/Commands/Tags/TagCommand.cs
@@ -12,7 +12,7 @@ namespace WinTenBot.Handlers.Commands.Tags
 {
     public class TagCommand : CommandBase
     {
-        private TagsService _tagsService;
+        private readonly TagsService _tagsService;
         private TelegramService _telegramService;
 
         public TagCommand()
@@ -47,84 +47,84 @@ namespace WinTenBot.Handlers.Commands.Tags
                        "\n📝 <i>Jika untuk grup, di rekomendasikan membuat sebuah channel, " +
                        "lalu link ke post di tautkan sebagai tombol.</i>";
 
-            if (msg.ReplyToMessage != null)
-            {
-                Log.Information("Replied message detected..");
-
-                var msgText = msg.Text;
-
-                var repMsg = msg.ReplyToMessage;
-                var repFileId = repMsg.GetFileId();
-                var repMsgText = repMsg.Text;
-                var partsMsgText = msgText.SplitText(" ").ToArray();
-
-                Log.Information($"Part1: {partsMsgText.ToJson(true)}");
-
-                var slugTag = partsMsgText.ValueOfIndex(1);
-                var tagAndCmd = partsMsgText.Take(2).ToArray();
-                var buttonData = msgText.RemoveThisString(tagAndCmd);
-
-                if (slugTag.Length >= 3)
-                {
-                    await _telegramService.SendTextAsync("📖 Sedang mempersiapkan..")
-                        .ConfigureAwait(false);
-
-                    var content = repMsg.Text ?? repMsg.Caption ?? "";
-                    Log.Information(content);
-
-                    bool isExist = await _tagsService.IsExist(msg.Chat.Id, args[0].Trim())
-                        .ConfigureAwait(false);
-                    Log.Information($"Tag isExist: {isExist}");
-                    if (!isExist)
-                    {
-                        var data = new Dictionary<string, object>()
-                        {
-                            {"chat_id", msg.Chat.Id},
-                            {"from_id", msg.From.Id},
-                            {"tag", slugTag.Trim()},
-                            {"btn_data", buttonData},
-                            {"content", content}
-                        };
-
-                        if (repFileId.IsNotNullOrEmpty())
-                        {
-                            data.Add("file_id", repFileId);
-                            data.Add("type_data", repMsg.Type);
-                        }
-
-                        await _telegramService.EditAsync("📝 Menyimpan tag data..")
-                            .ConfigureAwait(false);
-                        await _tagsService.SaveTagAsync(data)
-                            .ConfigureAwait(false);
-
-                        // var keyboard = new InlineKeyboardMarkup(
-                        //     InlineKeyboardButton.WithCallbackData("OK", "tag finish-create")
-                        // );
-
-                        await _telegramService.EditAsync("✅ Tag berhasil di simpan.." +
-                                                         $"\nTag: <code>#{slugTag}</code>" +
-                                                         $"\n\nKetik /tags untuk melihat semua Tag.")
-                            .ConfigureAwait(false);
-
-                        await _tagsService.UpdateCacheAsync(msg)
-                            .ConfigureAwait(false);
-                        return;
-                    }
-
-                    await _telegramService.EditAsync("✅ Tag sudah ada. " +
-                                                     "Silakan ganti Tag jika ingin isi konten berbeda")
-                        .ConfigureAwait(false);
-                    return;
-                }
-
-                await _telegramService.EditAsync("Slug Tag minimal 3 karakter")
-                    .ConfigureAwait(false);
-            }
-            else
+            if (msg.ReplyToMessage == null)
             {
                 await _telegramService.SendTextAsync(sendText)
                     .ConfigureAwait(false);
+                return;
             }
+
+            Log.Information("Replied message detected..");
+
+            var msgText = msg.Text.GetTextWithoutCmd();
+
+            var repMsg = msg.ReplyToMessage;
+            var repFileId = repMsg.GetFileId();
+            var repMsgText = repMsg.Text;
+            var partsMsgText = msgText.SplitText(" ").ToArray();
+
+            Log.Information($"Part1: {partsMsgText.ToJson(true)}");
+
+            var slugTag = partsMsgText.ValueOfIndex(0);
+            var tagAndCmd = partsMsgText.Take(2);
+            var buttonData = msgText.TrimStart(slugTag.ToCharArray()).Trim();
+
+            if (slugTag.Length < 3)
+            {
+                await _telegramService.EditAsync("Slug Tag minimal 3 karakter")
+                    .ConfigureAwait(false);
+                return;
+            }
+
+            await _telegramService.SendTextAsync("📖 Sedang mempersiapkan..")
+                .ConfigureAwait(false);
+
+            var content = repMsg.Text ?? repMsg.Caption ?? "";
+            Log.Information(content);
+
+            bool isExist = await _tagsService.IsExist(msg.Chat.Id, slugTag)
+                .ConfigureAwait(false);
+            Log.Information($"Tag isExist: {isExist}");
+            if (isExist)
+            {
+                await _telegramService.EditAsync("✅ Tag sudah ada. " +
+                                                 "Silakan ganti Tag jika ingin isi konten berbeda")
+                    .ConfigureAwait(false);
+                return;
+            }
+
+            var data = new Dictionary<string, object>()
+            {
+                {"chat_id", msg.Chat.Id},
+                {"from_id", msg.From.Id},
+                {"tag", slugTag.Trim()},
+                {"btn_data", buttonData},
+                {"content", content}
+            };
+
+            if (repFileId.IsNotNullOrEmpty())
+            {
+                data.Add("file_id", repFileId);
+                data.Add("type_data", repMsg.Type);
+            }
+
+            await _telegramService.EditAsync("📝 Menyimpan tag data..")
+                .ConfigureAwait(false);
+            await _tagsService.SaveTagAsync(data)
+                .ConfigureAwait(false);
+
+            // var keyboard = new InlineKeyboardMarkup(
+            //     InlineKeyboardButton.WithCallbackData("OK", "tag finish-create")
+            // );
+
+            await _telegramService.EditAsync("✅ Tag berhasil di simpan.." +
+                                             $"\nTag: <code>#{slugTag}</code>" +
+                                             $"\n\nKetik /tags untuk melihat semua Tag.")
+                .ConfigureAwait(false);
+
+            await _tagsService.UpdateCacheAsync(msg)
+                .ConfigureAwait(false);
+            return;
         }
     }
 }
