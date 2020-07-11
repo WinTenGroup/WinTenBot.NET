656e84781a3f9f41c826e32b8a069a5ecb6e481b
Azhe Kun <azhe403@gmail.com>
2020-07-11 13:58:21 +0700

    fix getting filename

--- src/WinTenBot/WinTenBot/Common/Url.cs
+++ src/WinTenBot/WinTenBot/Common/Url.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
-using Flurl;
 using Serilog;
 using WinTenBot.IO;
 using WinTenBot.Model;
@@ -50,5 +50,156 @@ namespace WinTenBot.Common
         {
             return Flurl.Url.IsValid(urlPath);
         }
+
+        public static bool IsUrlRedirected(this string url)
+        {
+            var req = (HttpWebRequest) HttpWebRequest.Create(url);
+            req.Method = "HEAD";
+            req.AllowAutoRedirect = false;
+            var resp = (HttpWebResponse) req.GetResponse();
+
+            return resp.StatusCode == HttpStatusCode.Redirect ||
+                   resp.StatusCode == HttpStatusCode.MovedPermanently ||
+                   resp.StatusCode == HttpStatusCode.RedirectKeepVerb ||
+                   resp.StatusCode == HttpStatusCode.RedirectMethod;
+        }
+
+        public static string GetRedirectedUrl(this string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return url;
+
+            Log.Information("Trying get redirected url: {0}", url);
+            int maxRedirCount = 8; // prevent infinite loops
+            string newUrl = url;
+            do
+            {
+                HttpWebRequest webRequest = null;
+                HttpWebResponse webResponse = null;
+                try
+                {
+                    webRequest = (HttpWebRequest) HttpWebRequest.Create(url);
+                    webRequest.Method = "HEAD";
+                    webRequest.AllowAutoRedirect = true;
+                    webResponse = (HttpWebResponse) webRequest.GetResponse();
+                    var statusCode = (int)webResponse.StatusCode;
+                    Log.Debug("Response: {0}", webResponse.ToJson(true));
+
+                    if (statusCode >= 300 && statusCode <= 399)
+                    {
+                        Log.Debug("Getting redirected from Headers");
+
+                        // newUrl = webResponse.Headers["Location"];
+                        if (newUrl == null)
+                            return url;
+
+                        if (newUrl.IndexOf("://", StringComparison.Ordinal) == -1)
+                        {
+                            // Doesn't have a URL Schema, meaning it's a relative or absolute URL
+                            Uri u = new Uri(new Uri(url), newUrl);
+                            newUrl = u.ToString();
+                        }
+
+                        Log.Debug("Finish redirect.");
+                    }
+                    
+                    newUrl = webResponse.ResponseUri.AbsoluteUri;
+
+                    // switch (resp.StatusCode)
+                    // {
+                    //     case HttpStatusCode.OK:
+                    //         return newUrl;
+                    //     case HttpStatusCode.Redirect:
+                    //     case HttpStatusCode.MovedPermanently:
+                    //     case HttpStatusCode.RedirectKeepVerb:
+                    //     case HttpStatusCode.RedirectMethod:
+                    //         Log.Debug("Getting redirected from Headers");
+                    //
+                    //         newUrl = resp.Headers["Location"];
+                    //         if (newUrl == null)
+                    //             return url;
+                    //
+                    //         if (newUrl.IndexOf("://", System.StringComparison.Ordinal) == -1)
+                    //         {
+                    //             // Doesn't have a URL Schema, meaning it's a relative or absolute URL
+                    //             Uri u = new Uri(new Uri(url), newUrl);
+                    //             newUrl = u.ToString();
+                    //         }
+                    //
+                    //         Log.Debug("Finish get.");
+                    //         break;
+                    //     default:
+                    //         return newUrl;
+                    // }
+
+                    url = newUrl;
+                }
+                catch (WebException ex)
+                {
+                    // Return the last known good URL
+                    Log.Error(ex.Demystify(), "Error WebException when try get redirectedUrl");
+                    return newUrl;
+                }
+                catch (Exception ex)
+                {
+                    Log.Error(ex.Demystify(), "Error when try get redirectedUrl");
+                    return null;
+                }
+                finally
+                {
+                    if (webResponse != null)
+                        webResponse.Close();
+                }
+            } while (maxRedirCount-- > 0);
+            
+            Log.Debug("Redirected URL: {0}", url);
+            return url;
+        }
+
+        public static Uri GetAutoRedirectedUrl(this string url)
+        {
+            var webRequest = (HttpWebRequest) HttpWebRequest.Create(url);
+            webRequest.Method = "HEAD";
+            webRequest.AllowAutoRedirect = true;
+            var webResponse = (HttpWebResponse) webRequest.GetResponse();
+            // var statusCode = (int)webResponse.StatusCode;
+            Log.Debug("Response: {0}", webResponse.ToJson(true));
+
+            return webResponse.ResponseUri;
+        }
+        
+        // public static bool MakeRequest(Uri url)
+        // {
+        //     using var client = new HttpClient(new HttpClientHandler()
+        //     {
+        //         AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip
+        //     }) { Timeout = TimeSpan.FromSeconds(30) };
+        //     var request = new HttpRequestMessage()
+        //     {
+        //         RequestUri = url,
+        //         Method = HttpMethod.Get
+        //     };
+        //
+        //     HttpResponseMessage response = client.SendAsync(request).Result;
+        //     var statusCode = (int)response.StatusCode;
+        //
+        //     // We want to handle redirects ourselves so that we can determine the final redirect Location (via header)
+        //     if (statusCode >= 300 && statusCode <= 399)
+        //     {
+        //         var redirectUri = response.Headers.Location;
+        //         if (!redirectUri.IsAbsoluteUri)
+        //         {
+        //             redirectUri = new Uri(request.RequestUri.GetLeftPart(UriPartial.Authority) + redirectUri);
+        //         }
+        //         _status.AddStatus($"Redirecting to {redirectUri}");
+        //         return MakeRequest(redirectUri);
+        //     }
+        //     else if (!response.IsSuccessStatusCode)
+        //     {
+        //         throw new Exception();
+        //     }
+        //
+        //     return true;
+        // }
     }
 }
--- src/WinTenBot/WinTenBot/Tools/Transfer.cs
+++ src/WinTenBot/WinTenBot/Tools/Transfer.cs
@@ -7,7 +7,6 @@ using WinTenBot.Common;
 using WinTenBot.Model;
 using WinTenBot.Services;
 using WinTenBot.Tools.GoogleCloud;
-using Url = Flurl.Url;
 
 namespace WinTenBot.Tools
 {
@@ -15,9 +14,8 @@ namespace WinTenBot.Tools
     {
         public static void AddQueue()
         {
-            
         }
-        
+
         public static string DownloadFile(this TelegramService telegramService, string address)
         {
             var message = telegramService.Message;
@@ -31,8 +29,9 @@ namespace WinTenBot.Tools
             var uri = new Uri(address);
 
             var cachePath = BotSettings.PathCache;
-            var url = new Url(address);
-            var fileName = Path.GetFileName(url.Path);
+            // var url = new Url(address.GetAutoRedirectedUrl());
+            var url = address.GetAutoRedirectedUrl();
+            var fileName = Path.GetFileName(url.AbsoluteUri);
             var saveFilePath = Path.Combine(cachePath, chatId.ToString(), fileName);
 
             swUpdater.Start();
@@ -48,7 +47,8 @@ namespace WinTenBot.Tools
                 var downloadSpeed = (args.BytesReceived / swProgress.Elapsed.TotalSeconds).SizeFormat("/s");
                 totalSize = args.TotalBytesToReceive.ToDouble().SizeFormat();
                 var progressText = $"Downloading file to Temp" +
-                                   $"\n<b>Url:</b> {address}" +
+                                   $"\n<b>Url:</b> {url.AbsoluteUri}" +
+                                   $"\n<b>Referrer:</b> {address}" +
                                    $"\n<b>Name:</b> {fileName}" +
                                    $"\n<b>Progress:</b> {progressPercentage} %" +
                                    $"\n<b>Size:</b> {totalSize}" +
@@ -71,7 +71,9 @@ namespace WinTenBot.Tools
                     .ConfigureAwait(false);
                 Log.Information($"Download file complete {args.Cancelled}");
 
-                await telegramService.EditAsync("Preparing upload {0} to Google Drive.", fileName)
+                var preparingUpload = $"Preparing upload file to Google Drive." +
+                                      $"\nFile: {fileName}";
+                await telegramService.EditAsync(preparingUpload)
                     .ConfigureAwait(false);
                 telegramService.UploadFile(saveFilePath);
             };
