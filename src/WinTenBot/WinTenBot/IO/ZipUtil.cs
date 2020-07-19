3e05672dcbeab063b82ab3ca413f3e923d47f416
Azhe Kun <azhe403@gmail.com>
2020-07-19 11:13:15 +0700

    add createZip

---
+++ src/WinTenBot/WinTenBot/IO/ZipUtil.cs
@@ -0,0 +1,32 @@
+﻿using Ionic.Zip;
+using Ionic.Zlib;
+using Serilog;
+
+namespace WinTenBot.IO
+{
+    public static class ZipUtil
+    {
+        public static string CreateZip(this string fileName, string saveTo)
+        {
+            Log.Information("Creating .zip from file {0}", fileName);
+            using var zip = new ZipFile
+            {
+                CompressionLevel = CompressionLevel.BestCompression
+            };
+
+            zip.AddFile(fileName,"");
+
+            Log.Debug("Saving to {0}", saveTo);
+            zip.Save(saveTo);
+
+            return saveTo;
+        }
+
+        public static string CreateZip(this string filePath)
+        {
+            var newFilePath = filePath.ReplaceExt(".zip");
+            var zipFile = filePath.CreateZip(newFilePath);
+            return zipFile;
+        }
+    }
+}
