6f370d4a72c743ce269b39aaed64b0a04db50556
Azhe-kun <azhe403@gmail.com>
2020-08-15 13:22:47 +0700

    NotFound no longer throw to exception

--- src/WinTenBot/WinTenBot/Providers/AntiSpamProvider.cs
+++ src/WinTenBot/WinTenBot/Providers/AntiSpamProvider.cs
@@ -17,9 +17,9 @@ namespace WinTenBot.Providers
     {
         public static async Task<SpamWatch> CheckSpamWatch(this int userId)
         {
-            var spamWatch  = new SpamWatch();
+            var spamWatch = new SpamWatch();
             var spamWatchToken = BotSettings.SpamWatchToken;
-            
+
             try
             {
                 var baseUrl = $"https://api.spamwat.ch/banlist/{userId}";
@@ -28,11 +28,12 @@ namespace WinTenBot.Providers
                     .GetJsonAsync<SpamWatch>()
                     .ConfigureAwait(false);
                 spamWatch.IsBan = spamWatch.Code != 404;
-                Log.Debug(spamWatch.ToJson(true));
+                Log.Debug("SpamWatch Result: {0}", spamWatch.ToJson(true));
             }
             catch (FlurlHttpException ex)
             {
                 var callHttpStatus = ex.Call.HttpStatus;
+                Log.Information("StatusCode: {0}", callHttpStatus);
                 switch (callHttpStatus)
                 {
                     case HttpStatusCode.NotFound:
@@ -40,47 +41,44 @@ namespace WinTenBot.Providers
                         break;
                     case HttpStatusCode.Unauthorized:
                         Log.Warning("Please check your SpamWatch API Token!");
+                        Log.Error(ex, "SpamWatch API FlurlHttpEx");
                         break;
                 }
-
-                Log.Information($"StatusCode: {callHttpStatus}");
-                Log.Error(ex, "SpamWatch API FlurlHttpEx");
             }
             catch (Exception ex)
             {
-                Log.Error(ex,"SpamWatch Exception");
+                Log.Error(ex, "SpamWatch Exception");
             }
 
             return spamWatch;
         }
-        
+
         public static async Task<bool> CheckGBan(this int userId)
         {
             var query = await new Query("fban_user")
-                .Where("user_id",userId)
+                .Where("user_id", userId)
                 .ExecForSqLite(true)
                 .GetAsync()
                 .ConfigureAwait(false);
-            
+
             var isGBan = query.Any();
             Log.Information($"UserId {userId} isGBan : {isGBan}");
-            
+
             return isGBan;
         }
-        
+
         public static async Task<bool> IsCasBanAsync(this User user)
         {
             var userId = user.Id;
             var url = "https://api.cas.chat/check".SetQueryParam("user_id", userId);
             var resp = await url.GetJsonAsync<CasBan>()
                 .ConfigureAwait(false);
-            
+
             Log.Debug("CasBan Response", resp);
 
             var isBan = resp.Ok;
             Log.Information($"UserId: {userId} is CAS ban: {isBan}");
             return isBan;
-
         }
     }
 }
