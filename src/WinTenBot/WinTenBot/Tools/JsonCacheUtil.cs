93fd22a6f7de9c758705b2b99bb8534b285ea815
Azhe-kun <azhe403@gmail.com>
2020-08-29 15:30:12 +0700

    add JsonCache

---
+++ src/WinTenBot/WinTenBot/Tools/JsonCacheUtil.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using JsonFlatFileDataStore;
+using Serilog;
+using WinTenBot.IO;
+
+namespace WinTenBot.Tools
+{
+    public static class JsonCacheUtil
+    {
+        private static string BasePath { get; } = Path.Combine("Storage", "JsonCache");
+
+        public static DataStore OpenJson(this string path)
+        {
+            var file = Path.Combine(BasePath, path + ".json").SanitizeSlash().EnsureDirectory();
+            Log.Information("Opening Json: {0}", file);
+            var store = new DataStore(file);
+            return store;
+        }
+
+        public static async Task<IDocumentCollection<T>> GetCollectionAsync<T>(this DataStore dataStore) where T : class
+        {
+            return await Task.Run(() =>
+            {
+                var collection = dataStore.GetCollection<T>();
+                return collection;
+            }).ConfigureAwait(false);
+
+        }
+    }
+}
