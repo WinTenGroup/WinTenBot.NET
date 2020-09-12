f8dba6b8c74a439d01177569bb9c4cddfc4620fb
Azhe-kun <azhe403@gmail.com>
2020-09-12 14:54:00 +0700

    add gban sync cache

---
+++ src/WinTenBot/WinTenBot/Handlers/Commands/GlobalBan/GlobalBanSyncCommand.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Framework.Abstractions;
+using WinTenBot.Services;
+using WinTenBot.Telegram;
+using WinTenBot.Tools;
+
+namespace WinTenBot.Handlers.Commands.GlobalBan
+{
+    public class GlobalBanSyncCommand : CommandBase
+    {
+        private TelegramService _telegramService;
+
+        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
+            CancellationToken cancellationToken)
+        {
+            _telegramService = new TelegramService(context);
+            
+            if(!_telegramService.IsSudoer()) return;
+
+            await _telegramService.SendTextAsync("Sedang sinkronisasi..")
+                .ConfigureAwait(false);
+
+            var rowCount = await Sync.SyncGBanToLocalAsync()
+                .ConfigureAwait(false);
+
+            await _telegramService.EditAsync($"Sinkronisasi sebanyak {rowCount} selesai")
+                .ConfigureAwait(false);
+        }
+    }
+}
