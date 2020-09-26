4d45792ef86f107bb4b156924d8dff1544c1b229
Azhe-kun <azhe403@gmail.com>
2020-09-26 10:47:26 +0700

    add stickerpack

--- src/WinTenBot/WinTenBot/IO/ZipUtil.cs
+++ src/WinTenBot/WinTenBot/IO/ZipUtil.cs
@@ -1,4 +1,8 @@
-﻿using Ionic.Zip;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Ionic.Zip;
 using Ionic.Zlib;
 using Serilog;
 
@@ -14,8 +18,41 @@ namespace WinTenBot.IO
                 CompressionLevel = CompressionLevel.BestCompression
             };
 
-            zip.AddFile(fileName,"");
+            if (fileName.IsDirectory())
+            {
+                var files = Directory.GetFiles(fileName, "*.*", SearchOption.AllDirectories)
+                    .Where(x => !x.Contains(".stikerpacks"));
+                // zip.AddFiles(files, String.Empty);
+                foreach (var file in files)
+                {
+                    zip.AddFile(file, Path.GetDirectoryName(file));
+                }
 
+                // zip.AddDirectory(fileName);
+            }
+            else
+            {
+                zip.AddFile(fileName, "");
+            }
+
+            Log.Debug("Saving to {0}", saveTo);
+            zip.Save(saveTo);
+
+            return saveTo;
+        }
+
+        public static string CreateZip(this IEnumerable<string> listPath, string saveTo)
+        {
+            using var zip = new ZipFile
+            {
+                CompressionLevel = CompressionLevel.BestCompression
+            };
+            
+            foreach (var file in listPath)
+            {
+                zip.AddFile(file, Path.GetDirectoryName(file));
+            }
+            
             Log.Debug("Saving to {0}", saveTo);
             zip.Save(saveTo);
 
