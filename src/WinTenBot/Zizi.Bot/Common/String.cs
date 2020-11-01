69cc4ccbb2aa193974f243d743ca15df9446f22b
Azhe-kun <azhe403@gmail.com>
2020-11-01 16:32:16 +0700

    add unique id generator

--- src/WinTenBot/Zizi.Bot/Common/String.cs
+++ src/WinTenBot/Zizi.Bot/Common/String.cs
@@ -251,5 +251,23 @@ namespace Zizi.Bot.Common
 
             return string.Format($"{Math.Round(size, 2)} {norm[x]}{suffix}", MidpointRounding.AwayFromZero);
         }
+
+        public static string GenerateUniqueId()
+        {
+            var builder = new StringBuilder();
+            Enumerable
+                .Range(65, 26)
+                .Select(e => ((char)e).ToString())
+                .Concat(Enumerable.Range(97, 26).Select(e => ((char)e).ToString()))
+                .Concat(Enumerable.Range(0, 10).Select(e => e.ToString()))
+                .OrderBy(e => Guid.NewGuid())
+                .Take(11)
+                .ToList()
+                .ForEach(e => builder.Append(e));
+            
+            var id = builder.ToString();
+
+            return id;
+        }
     }
 }
