2d1fa8e2f5ef1e747c3d4f94d1a4ce2400fac378
Azhe-kun <azhe403@gmail.com>
2020-12-20 23:54:29 +0700

    change column to nullable

--- src/WinTenBot/Zizi.Bot/Migrations/MySql/CreateTableAfk.cs
+++ src/WinTenBot/Zizi.Bot/Migrations/MySql/CreateTableAfk.cs
@@ -3,10 +3,10 @@
 namespace Zizi.Bot.Migrations.MySql
 {
     [Migration(120200705202114)]
-    public class CreateTableAfk:Migration
+    public class CreateTableAfk : Migration
     {
         private const string TableName = "afk";
-        
+
         public override void Up()
         {
             if (Schema.Table(TableName).Exists()) return;
@@ -15,7 +15,7 @@ namespace Zizi.Bot.Migrations.MySql
                 .WithColumn("id").AsInt16().PrimaryKey().Identity()
                 .WithColumn("user_id").AsString(15)
                 .WithColumn("chat_id").AsString(30)
-                .WithColumn("afk_reason").AsCustom("TEXT")
+                .WithColumn("afk_reason").AsCustom("TEXT").Nullable()
                 .WithColumn("is_afk").AsBoolean().WithDefaultValue(0)
                 .WithColumn("afk_start").AsDateTime().WithDefault(SystemMethods.CurrentDateTime)
                 .WithColumn("afk_end").AsDateTime().WithDefault(SystemMethods.CurrentDateTime);
