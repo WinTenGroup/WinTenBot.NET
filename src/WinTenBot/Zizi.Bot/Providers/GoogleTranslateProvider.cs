ee2c3a438b70806e649ef604fb0754e4ad719201
Azhe-kun <azhe403@gmail.com>
2020-12-29 14:00:35 +0700

    add merged translation

--- src/WinTenBot/Zizi.Bot/Providers/GoogleTranslateProvider.cs
+++ src/WinTenBot/Zizi.Bot/Providers/GoogleTranslateProvider.cs
@@ -5,17 +5,21 @@ namespace Zizi.Bot.Providers
 {
     public static class GoogleTranslateProvider
     {
-
         public static async Task<TranslationResult> TranslateAsync(this string forTranslate, string toLang)
         {
             var translator = new GoogleTranslator();
             var from = Language.Auto;
             var to = GoogleTranslator.GetLanguageByISO(toLang);
 
-            var result = await translator.TranslateLiteAsync(forTranslate, from, to)
+            var result = await translator.TranslateAsync(forTranslate, from, to)
                 .ConfigureAwait(false);
 
             return result;
+        }
+
+        public static async Task<string> MergedTranslateAsync(this string forTranslate, string toLang)
+        {
+            return (await forTranslate.TranslateAsync(toLang).ConfigureAwait(false)).MergedTranslation;
         }
     }
 }
