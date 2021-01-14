1fef90f67dd4889d8039d477e27de3e533d68498
Azhe-kun <azhe403@gmail.com>
2021-01-14 21:55:34 +0700

    handle error translation

--- src/WinTenBot/Zizi.Bot/Handlers/Commands/Additional/TranslateCommand.cs
+++ src/WinTenBot/Zizi.Bot/Handlers/Commands/Additional/TranslateCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Serilog;
 using Telegram.Bot.Framework.Abstractions;
@@ -8,49 +10,63 @@ using Zizi.Bot.Services;
 
 namespace Zizi.Bot.Handlers.Commands.Additional
 {
-    public class TranslateCommand:CommandBase
+    public class TranslateCommand : CommandBase
     {
         private TelegramService _telegramService;
-        
+
         public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args, CancellationToken cancellationToken)
-        { 
+        {
             _telegramService = new TelegramService(context);
 
             var message = _telegramService.Message;
             var userLang = message.From.LanguageCode;
 
-            if (message.ReplyToMessage != null)
+            // await _telegramService.SendTextAsync("ℹ️ Sorry, Translation temporary unavailable.")
+            //     .ConfigureAwait(false);
+            //
+            // return;
+
+            if (message.ReplyToMessage == null)
             {
-                var param = message.Text.SplitText(" ").ToArray();
-                var param1 = param.ValueOfIndex(1) ?? "";
-
-                // if (param1.IsNullOrEmpty() ||  !param1.Contains("-"))
-                // {
-                //     await _telegramProvider.SendTextAsync("Lang code di perlukan. Contoh: en-id, English ke Indonesia");
-                //     return;
-                // }
-
-                if (param1.IsNullOrEmpty())
-                {
-                    param1 = message.From.LanguageCode;
-                }
-                
-                var forTranslate = message.ReplyToMessage.Text;
-
-                Log.Information($"Param: {param.ToJson(true)}");
-
-                await _telegramService.SendTextAsync("🔄 Translating into Your language..")
+                var hint = await "Balas pesan yang ingin anda terjemahkan".TranslateAsync(userLang)
                     .ConfigureAwait(false);
+                await _telegramService.SendTextAsync(hint.MergedTranslation)
+                    .ConfigureAwait(false);
+                return;
+            }
 
+            var param = message.Text.SplitText(" ").ToArray();
+            var param1 = param.ValueOfIndex(1) ?? "";
+
+
+            // if (param1.IsNullOrEmpty() ||  !param1.Contains("-"))
+            // {
+            //     await _telegramProvider.SendTextAsync("Lang code di perlukan. Contoh: en-id, English ke Indonesia");
+            //     return;
+            // }
+
+            if (param1.IsNullOrEmpty())
+            {
+                param1 = message.From.LanguageCode;
+            }
+
+            var forTranslate = message.ReplyToMessage.Text;
+
+            Log.Information("Param: {0}", param.ToJson(true));
+
+            await _telegramService.SendTextAsync("🔄 Translating into Your language..")
+                .ConfigureAwait(false);
+            try
+            {
                 var translate = await forTranslate.TranslateAsync(param1)
                     .ConfigureAwait(false);
 
                 // var translate = forTranslate.TranslateTo(param1);
-                
+
                 // var translateResult = new StringBuilder();
                 // foreach (var translation in translate.Result.Translations)
                 // {
-                    // translateResult.AppendLine(translation._Translation);
+                // translateResult.AppendLine(translation._Translation);
                 // }
 
                 var translateResult = translate.MergedTranslation;
@@ -58,14 +74,15 @@ namespace Zizi.Bot.Handlers.Commands.Additional
                 await _telegramService.EditAsync(translateResult)
                     .ConfigureAwait(false);
             }
-            else
+            catch (Exception ex)
             {
-                var hint = await "Balas pesan yang ingin anda terjemahkan".TranslateAsync(userLang)
-                    .ConfigureAwait(false);
-                await _telegramService.SendTextAsync(hint.MergedTranslation)
+                Log.Error(ex.Demystify(), "Error translation");
+
+                var messageError = "Error translation" +
+                                   $"\nMessage: {ex.Message}";
+                await _telegramService.EditAsync(messageError)
                     .ConfigureAwait(false);
             }
-
         }
     }
 }
