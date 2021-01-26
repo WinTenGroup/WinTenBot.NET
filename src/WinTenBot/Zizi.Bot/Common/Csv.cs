ac8055ff6942a779b0cc61906b3e45b9ef2b0bb8
Azhe-kun <azhe403@gmail.com>
2021-01-27 00:17:15 +0700

    update CsvHelper and fix breaking changes

--- src/WinTenBot/Zizi.Bot/Common/Csv.cs
+++ src/WinTenBot/Zizi.Bot/Common/Csv.cs
@@ -12,37 +12,56 @@ namespace Zizi.Bot.Common
     {
         public static void Write<T>(string filePath, IEnumerable<T> records, string delimiter = ",")
         {
-            Log.Information($"Writing {records.Count()} rows to {filePath}");
+            var listRecord = records.ToList();
+            Log.Information("Writing {0} rows to {1}", listRecord.Count, filePath);
 
+            // v20 or above
             var config = new CsvConfiguration(CultureInfo.CurrentCulture)
             {
                 Delimiter = delimiter,
+                HasHeaderRecord = false
             };
 
             using var writer = new StreamWriter(filePath);
             using var csv = new CsvWriter(writer, config);
-            csv.Context.WriterConfiguration.HasHeaderRecord = false;
-            csv.WriteRecords(records);
+
+            // v19 or below
+            // csv.Context.WriterConfiguration.HasHeaderRecord = false;
+
+            csv.WriteRecords(listRecord);
+            Log.Debug("CSV file written to {0}", filePath);
         }
 
         public static IEnumerable<T> ReadCsv<T>(string filePath, bool hasHeader = true, string delimiter = ",")
         {
             if (!File.Exists(filePath))
             {
-                Log.Information($"File {filePath} is not exist");
+                Log.Information("File {0} is not exist", filePath);
                 return null;
             }
 
+            // v20 or above
+            var csvConfiguration = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = hasHeader,
+                Delimiter = delimiter,
+                MissingFieldFound = null,
+                BadDataFound = null,
+                PrepareHeaderForMatch = (header, index) => header.ToLower()
+            };
+
             using var reader = new StreamReader(filePath);
-            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-            csv.Configuration.HasHeaderRecord = hasHeader;
-            csv.Configuration.Delimiter = delimiter;
-            csv.Configuration.MissingFieldFound = null;
-            csv.Configuration.BadDataFound = null;
+            using var csv = new CsvReader(reader, csvConfiguration);
+
+            // v19 or below
+            // csv.Configuration.HasHeaderRecord = hasHeader;
+            // csv.Configuration.Delimiter = delimiter;
+            // csv.Configuration.MissingFieldFound = null;
+            // csv.Configuration.BadDataFound = null;
             // csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
 
             var records = csv.GetRecords<T>().ToList();
-            Log.Information($"Parsing csv records {records.Count} row(s)");
+            Log.Information("Parsing csv records {0} row(s)", records.Count);
 
             return records;
         }
