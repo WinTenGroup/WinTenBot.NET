7683f1f866864710474f70ab45aeb8f1a4dc4da2
Azhe-kun <azhe403@gmail.com>
2021-02-23 12:56:23 +0700

    move number format
    Signed-off-by: Azhe-kun <azhe403@gmail.com>

---
+++ src/WinTenBot/Zizi.Core/Utils/NumberUtil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Zizi.Core.Utils
+{
+    public static class NumberUtil
+    {
+        public static string SizeFormat(this double size, string suffix = null)
+        {
+            string[] norm = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+            int count = norm.Length - 1;
+            int x = 0;
+
+            while (size >= 1000 && x < count)
+            {
+                size /= 1024;
+                x++;
+            }
+
+            return string.Format($"{Math.Round(size, 2)} {norm[x]}{suffix}", MidpointRounding.ToZero);
+        }
+
+        public static string SizeFormat(this long size, string suffix = null)
+        {
+            var sizeD = Convert.ToDouble(size);
+            return SizeFormat(sizeD, suffix);
+
+        }
+
+        public static string NumberSeparator(this int number)
+        {
+            return number.ToString("N0", new CultureInfo("id-ID"));
+        }
+    }
+}
--- src/WinTenBot/Zizi.Core/Utils/Text/StringUtil.cs
+++ src/WinTenBot/Zizi.Core/Utils/Text/StringUtil.cs
@@ -237,22 +237,6 @@ namespace Zizi.Core.Utils.Text
             return new string(str.ToCharArray().Distinct().ToArray());
         }
 
-        public static string SizeFormat(this double size, string suffix = null)
-        {
-            string[] norm = {"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
-            int count = norm.Length - 1;
-            // var size = bytes;
-            int x = 0;
-
-            while (size >= 1000 && x < count)
-            {
-                size /= 1024;
-                x++;
-            }
-
-            return string.Format($"{Math.Round(size, 2)} {norm[x]}{suffix}", MidpointRounding.AwayFromZero);
-        }
-
         public static string GenerateUniqueId()
         {
             var builder = new StringBuilder();
