cbfde23a8866ebc9d7bc1c8d5d0163ebdfc1dfe3
Azhe-kun <azhe403@gmail.com>
2021-03-02 17:57:07 +0700

    delete function, please use from Zizi.Core

--- src/WinTenBot/Zizi.Bot/Common/Array.cs
+++ src/WinTenBot/Zizi.Bot/Common/Array.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Serilog;
@@ -63,43 +63,6 @@ namespace Zizi.Bot.Common
             }
 
             return result;
-        }
-
-
-        // Source: https://www.codeproject.com/Articles/44274/Transpose-a-DataTable-using-C
-        public static DataTable TransposedTable(this DataTable inputTable)
-        {
-            DataTable outputTable = new DataTable();
-
-            // Add columns by looping rows
-
-            // Header row's first column is same as in inputTable
-            outputTable.Columns.Add(inputTable.Columns[0].ColumnName);
-
-            // Header row's second column onwards, 'inputTable's first column taken
-            foreach (DataRow inRow in inputTable.Rows)
-            {
-                string newColName = inRow[0].ToString();
-                outputTable.Columns.Add(newColName);
-            }
-
-            // Add rows by looping columns        
-            for (int rCount = 1; rCount <= inputTable.Columns.Count - 1; rCount++)
-            {
-                DataRow newRow = outputTable.NewRow();
-
-                // First column is inputTable's Header row's second column
-                newRow[0] = inputTable.Columns[rCount].ColumnName;
-                for (int cCount = 0; cCount <= inputTable.Rows.Count - 1; cCount++)
-                {
-                    string colValue = inputTable.Rows[cCount][rCount].ToString();
-                    newRow[cCount + 1] = colValue;
-                }
-
-                outputTable.Rows.Add(newRow);
-            }
-
-            return outputTable;
         }
     }
 }
