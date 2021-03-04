61c404e75d56bf8459f36ca6da92ec1d268b8812
Azhe-kun <azhe403@gmail.com>
2021-03-04 07:46:34 +0700

    remove RavenDb embedded

--- src/WinTenBot/Zizi.Bot/Extensions/RavenDBServiceExtension.cs
+++ src/WinTenBot/Zizi.Bot/Extensions/RavenDBServiceExtension.cs
@@ -1,8 +1,6 @@
 using System.Security.Cryptography.X509Certificates;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Raven.Client.Documents;
-using Raven.Embedded;
 using Serilog;
 using Zizi.Bot.Models.Settings;
 
@@ -37,37 +35,37 @@ namespace Zizi.Bot.Extensions
             return services;
         }
 
-        public static IApplicationBuilder UseEmbeddedRavenDBServer(this IApplicationBuilder app)
-        {
-            var appConfig = app?.ApplicationServices.GetService<AppConfig>();
-            var ravenDbConfig = appConfig?.RavenDBConfig;
-            var serverUrl = ravenDbConfig?.Embedded.ServerUrl;
-            var frameworkVersion = ravenDbConfig?.Embedded.FrameworkVersion;
-
-            Log.Debug("Starting RavenDB Embedded server.");
-
-            EmbeddedServer.Instance.StartServer(new ServerOptions()
-            {
-                AcceptEula = true,
-                ServerUrl = serverUrl,
-                FrameworkVersion = frameworkVersion,
-                DataDirectory = "Storage/Data/",
-                LogsPath = "Storage/Logs/",
-            });
-
-            EmbeddedServer.Instance.ServerProcessExited += delegate(object sender, ServerProcessExitedEventArgs args)
-            {
-                Log.Error("Server Exited. {0} => {1}", sender, args);
-            };
-
-            var pid = EmbeddedServer.Instance.GetServerProcessIdAsync().Result;
-            var serverUri = EmbeddedServer.Instance.GetServerUriAsync().Result;
-
-            Log.Debug("RavenDB Embedded server initialized.");
-            Log.Debug("ProcessID: {0}", pid);
-            Log.Debug("ServerURL: {0}", serverUri);
-
-            return app;
-        }
+        // public static IApplicationBuilder UseEmbeddedRavenDBServer(this IApplicationBuilder app)
+        // {
+        //     var appConfig = app?.ApplicationServices.GetService<AppConfig>();
+        //     var ravenDbConfig = appConfig?.RavenDBConfig;
+        //     var serverUrl = ravenDbConfig?.Embedded.ServerUrl;
+        //     var frameworkVersion = ravenDbConfig?.Embedded.FrameworkVersion;
+        //
+        //     Log.Debug("Starting RavenDB Embedded server.");
+        //
+        //     EmbeddedServer.Instance.StartServer(new ServerOptions()
+        //     {
+        //         AcceptEula = true,
+        //         ServerUrl = serverUrl,
+        //         FrameworkVersion = frameworkVersion,
+        //         DataDirectory = "Storage/Data/",
+        //         LogsPath = "Storage/Logs/",
+        //     });
+        //
+        //     EmbeddedServer.Instance.ServerProcessExited += delegate(object sender, ServerProcessExitedEventArgs args)
+        //     {
+        //         Log.Error("Server Exited. {0} => {1}", sender, args);
+        //     };
+        //
+        //     var pid = EmbeddedServer.Instance.GetServerProcessIdAsync().Result;
+        //     var serverUri = EmbeddedServer.Instance.GetServerUriAsync().Result;
+        //
+        //     Log.Debug("RavenDB Embedded server initialized.");
+        //     Log.Debug("ProcessID: {0}", pid);
+        //     Log.Debug("ServerURL: {0}", serverUri);
+        //
+        //     return app;
+        // }
     }
 }
