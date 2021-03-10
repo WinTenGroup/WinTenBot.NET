d96eb4f1c51db3ddf52aad5bf1825a546b0d5ed6
Azhe-kun <azhe403@gmail.com>
2021-03-11 00:08:15 +0700

    cleanup migration

--- src/WinTenBot/Zizi.Bot/Migrations/MySql/CreateTableRssHistory.cs
+++ src/WinTenBot/Zizi.Bot/Migrations/MySql/CreateTableRssHistory.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using FluentMigrator;
+﻿using FluentMigrator;
 using Zizi.Bot.Extensions;
 
 namespace Zizi.Bot.Migrations.MySql
