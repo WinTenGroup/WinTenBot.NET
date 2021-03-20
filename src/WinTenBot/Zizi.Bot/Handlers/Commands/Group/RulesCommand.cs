f4a60be9c772015b1aaea0cc2b1f9c3533a26d70
Azhe Kun <azhe403@gmail.com>
2021-03-21 03:22:26 +0700

    move rules command

---
+++ src/WinTenBot/Zizi.Bot/Handlers/Commands/Group/RulesCommand.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Serilog;
+using Telegram.Bot.Framework.Abstractions;
+using Telegram.Bot.Types.Enums;
+using Zizi.Bot.Common;
+using Zizi.Bot.Services;
+using Zizi.Bot.Services.Datas;
+using Zizi.Bot.Telegram;
+
+namespace Zizi.Bot.Handlers.Commands.Group
+{
+    public class RulesCommand : CommandBase
+    {
+        private readonly SettingsService _settingsService;
+        private readonly TelegramService _telegramService;
+
+        public RulesCommand(SettingsService settingsService, TelegramService telegramService)
+        {
+            _settingsService = settingsService;
+            _telegramService = telegramService;
+        }
+
+        public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
+            CancellationToken cancellationToken)
+        {
+            await _telegramService.AddUpdateContext(context);
+            var msg = context.Update.Message;
+
+            var chatId = _telegramService.ChatId;
+
+            var sendText = "Under maintenance";
+            if (msg.Chat.Type == ChatType.Private)
+            {
+                Log.Debug("Rules only for Group");
+                return;
+            }
+
+            if (msg.From.Id.IsSudoer())
+            {
+                var settings = await _settingsService.GetSettingsByGroup(chatId);
+
+                Log.Information("Settings: {0}", settings.ToJson(true));
+                // var rules = settings.Rows[0]["rules_text"].ToString();
+                var rules = settings.RulesText;
+                Log.Debug("Rules: \n{0}", rules);
+                sendText = rules;
+            }
+
+            await _telegramService.SendTextAsync(sendText);
+        }
+    }
+}
