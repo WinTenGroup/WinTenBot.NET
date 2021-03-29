b1f4a7c3fefa393f123d401c6d7cac2cc426243b
Azhe-kun <azhe403@gmail.com>
2021-03-29 07:17:08 +0700

    change to nullable column

--- src/WinTenBot/Zizi.Bot/Migrations/MySql/CreateTableHitActivity.cs
+++ src/WinTenBot/Zizi.Bot/Migrations/MySql/CreateTableHitActivity.cs
@@ -17,14 +17,14 @@ namespace Zizi.Bot.Migrations.MySql
                 .WithColumn("via_bot").AsMySqlVarchar(128).Nullable()
                 .WithColumn("message_type").AsMySqlVarchar(100).Nullable()
                 .WithColumn("from_id").AsInt32()
-                .WithColumn("from_first_name").AsMySqlText()
-                .WithColumn("from_last_name").AsMySqlText()
-                .WithColumn("from_username").AsMySqlVarchar(128)
+                .WithColumn("from_first_name").AsMySqlText().Nullable()
+                .WithColumn("from_last_name").AsMySqlText().Nullable()
+                .WithColumn("from_username").AsMySqlVarchar(128).Nullable()
                 .WithColumn("from_lang_code").AsMySqlVarchar(20)
                 .WithColumn("chat_id").AsInt64()
-                .WithColumn("chat_username").AsMySqlVarchar(128)
+                .WithColumn("chat_username").AsMySqlVarchar(128).Nullable()
                 .WithColumn("chat_type").AsMySqlVarchar(25)
-                .WithColumn("chat_title").AsMySqlVarchar(128)
+                .WithColumn("chat_title").AsMySqlVarchar(128).Nullable()
                 .WithColumn("timestamp").AsMySqlTimestamp().WithDefault(SystemMethods.CurrentDateTime);
         }
 
