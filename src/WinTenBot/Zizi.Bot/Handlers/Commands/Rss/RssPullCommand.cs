dc39597507ca6451e08e6ed32fdff0a312dcf831
Azhe-kun <azhe403@gmail.com>
2021-04-13 21:41:25 +0700

    fix rsspull (manual rss update)

--- src/WinTenBot/Zizi.Bot/Handlers/Commands/Rss/RssPullCommand.cs
+++ src/WinTenBot/Zizi.Bot/Handlers/Commands/Rss/RssPullCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Serilog;
 using Telegram.Bot.Framework.Abstractions;
 using Zizi.Bot.Telegram;
 using Zizi.Bot.Services;
@@ -9,50 +10,50 @@ namespace Zizi.Bot.Handlers.Commands.Rss
 {
     public class RssPullCommand : CommandBase
     {
-        private TelegramService _telegramService;
+        private readonly TelegramService _telegramService;
+
+        public RssPullCommand(TelegramService telegramService)
+        {
+            _telegramService = telegramService;
+        }
 
         public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
             CancellationToken cancellationToken)
         {
-            // ChatHelper.Init(context);
-            _telegramService = new TelegramService(context);
-
-            // var chatId = ChatHelper.Message.Chat.Id.ToString();
-            // var isAdmin = await ChatHelper.IsAdminGroup();
+            await _telegramService.AddUpdateContext(context);
 
             var chatId = _telegramService.Message.Chat.Id;
-            var isAdmin = await _telegramService.IsAdminGroup()
-                .ConfigureAwait(false);
+            var isAdmin = await _telegramService.IsAdminGroup();
 
-            if (isAdmin || _telegramService.IsPrivateChat())
+            if (!isAdmin && !_telegramService.IsPrivateChat)
             {
-#pragma warning disable 4014
-                Task.Run(async () =>
-#pragma warning restore 4014
-                {
-                    // Thread.CurrentThread.IsBackground = true;
+                Log.Warning("You must Admin or Private chat");
 
-                    await _telegramService.SendTextAsync("Sedang memeriksa RSS feed baru..")
-                        .ConfigureAwait(false);
-                    // await "Sedang memeriksa RSS feed baru..".SendTextAsync();
-
-                    var newRssCount = await RssFeedUtil.ExecBroadcasterAsync(chatId)
-                        .ConfigureAwait(false);
-                    if (newRssCount == 0)
-                    {
-                        await _telegramService.EditAsync("Tampaknya tidak ada RSS baru saat ini")
-                            .ConfigureAwait(false);
-                        // await "Tampaknya tidak ada RSS baru saat ini".EditAsync();
-                    }
-                    else
-                    {
-                        await _telegramService.DeleteAsync(_telegramService.SentMessageId)
-                            .ConfigureAwait(false);
-                    }
-
-                    // ChatHelper.Close();
-                }, cancellationToken);
+                return;
             }
+
+            Log.Information("Pulling RSS in {0}", chatId);
+
+#pragma warning disable 4014
+            Task.Run(async () =>
+#pragma warning restore 4014
+            {
+                await _telegramService.SendTextAsync("Sedang memeriksa RSS feed baru..");
+
+                var reducedChatId = chatId.ReduceChatId();
+                var recurringId = $"rss-{reducedChatId}";
+                var count = HangfireUtil.TriggerJobs(recurringId);
+
+                if (count == 0)
+                {
+                    await _telegramService.EditAsync("Tampaknya tidak ada RSS baru saat ini");
+                }
+                else
+                {
+                    await _telegramService.DeleteAsync(_telegramService.SentMessageId);
+                }
+
+            }, cancellationToken);
         }
     }
 }
