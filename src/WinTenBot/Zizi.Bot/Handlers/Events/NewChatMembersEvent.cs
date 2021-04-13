6aec7ef451300394d72c7dacd0b8d0b224686eb4
Azhe-kun <azhe403@gmail.com>
2021-04-14 05:09:21 +0700

    fix new members event

--- src/WinTenBot/Zizi.Bot/Handlers/Events/NewChatMembersEvent.cs
+++ src/WinTenBot/Zizi.Bot/Handlers/Events/NewChatMembersEvent.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -7,160 +8,183 @@ using Serilog;
 using Telegram.Bot.Framework.Abstractions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
-using Zizi.Bot.Providers;
 using Zizi.Bot.Common;
 using Zizi.Bot.Enums;
 using Zizi.Bot.Models;
+using Zizi.Bot.Models.Settings;
+using Zizi.Bot.Providers;
 using Zizi.Bot.Services;
+using Zizi.Bot.Services.Datas;
 using Zizi.Bot.Telegram;
 
 namespace Zizi.Bot.Handlers.Events
 {
     public class NewChatMembersEvent : IUpdateHandler
     {
-        private SettingsService _settingsService;
-        private TelegramService _telegramService;
+        private readonly AppConfig _appConfig;
+        private readonly EnginesConfig _enginesConfig;
+        private readonly SettingsService _settingsService;
+        private readonly TelegramService _telegramService;
         private ChatSetting Settings { get; set; }
+
+        public NewChatMembersEvent(
+            AppConfig appConfig,
+            EnginesConfig enginesConfig,
+            SettingsService settingsService,
+            TelegramService telegramService
+        )
+        {
+            _appConfig = appConfig;
+            _enginesConfig = enginesConfig;
+            _settingsService = settingsService;
+            _telegramService = telegramService;
+        }
 
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
+            var stopwatch = Stopwatch.StartNew();
+
+            await _telegramService.AddUpdateContext(context);
+
             var msg = context.Update.Message;
-            _telegramService = new TelegramService(context);
-            _settingsService = new SettingsService(msg);
-            await _telegramService.DeleteAsync(msg.MessageId)
-                .ConfigureAwait(false);
+            var chatId = _telegramService.ChatId;
+
+            await _telegramService.DeleteAsync(msg.MessageId);
 
             Log.Information("New Chat Members...");
 
-            var chatSettings = _telegramService.CurrentSetting;
-            Settings = chatSettings;
+            Settings = await _settingsService.GetSettingsByGroup(chatId);
 
-            if (!chatSettings.EnableWelcomeMessage)
+            if (!Settings.EnableWelcomeMessage)
             {
                 Log.Information("Welcome message is disabled!");
                 return;
             }
 
+            var welcomeMessage = Settings.WelcomeMessage;
+            var welcomeButton = Settings.WelcomeButton;
+
             var newMembers = msg.NewChatMembers;
-            // var isBootAdded = await newMembers.IsBotAdded().ConfigureAwait(false);
-            var isBootAdded = await _telegramService.IsBotAdded(newMembers).ConfigureAwait(false);
+
+            var isBootAdded = await _telegramService.IsBotAdded(newMembers);
             if (isBootAdded)
             {
-                var isRestricted = await _telegramService.EnsureChatRestrictionAsync()
-                    .ConfigureAwait(false);
+                var isRestricted = await _telegramService.EnsureChatRestrictionAsync();
                 if (isRestricted) return;
 
-                var botName = BotSettings.GlobalConfiguration["Engines:ProductName"];
-                var sendText = $"Hai, perkenalkan saya {botName}" +
-                               $"\nFYI saya di bangun ulang menggunakan .NET." +
-                               $"\n\nAku adalah bot pendebug dan grup manajemen yang di lengkapi dengan alat keamanan. " +
-                               $"Agar saya berfungsi penuh, jadikan saya admin dengan level standard. " +
-                               $"\n\nAku akan menerapkan konfigurasi standard jika aku baru pertama kali masuk kesini. " +
-                               $"\n\nUntuk melihat daftar perintah bisa ketikkan /help";
+                var getMe = await _telegramService.GetMeAsync();
 
-                await _telegramService.SendTextAsync(sendText).ConfigureAwait(false);
+                var botName = _enginesConfig.ProductName;
+                var greetMe = $"Hai, perkenalkan saya {getMe.FirstName}" +
+
+                              $"\n\nSaya adalah bot pendebug dan grup manajemen yang di lengkapi dengan alat keamanan. " +
+                              $"Agar saya berfungsi penuh, jadikan saya admin dengan level standard. " +
+
+                              $"\n\nUntuk melihat daftar perintah bisa ketikkan /help";
+
+                await _telegramService.SendTextAsync(greetMe, replyToMsgId: 0);
                 await _settingsService.SaveSettingsAsync(new Dictionary<string, object>()
                 {
                     {"chat_id", msg.Chat.Id},
                     {"chat_title", msg.Chat.Title}
-                }).ConfigureAwait(false);
+                });
 
                 if (newMembers.Length == 1) return;
             }
 
-            var parsedNewMember = await ParseMemberCategory(newMembers).ConfigureAwait(false);
+            var parsedNewMember = await ParseMemberCategory(newMembers);
             var allNewMember = parsedNewMember.AllNewMember;
             var allNoUsername = parsedNewMember.AllNoUsername;
             var allNewBot = parsedNewMember.AllNewBot;
 
-            if (allNewMember.Length > 0)
+            if (allNewMember.Length <= 0)
             {
-                var chatTitle = msg.Chat.Title;
-                var greet = Time.GetTimeGreet();
-                var memberCount = await _telegramService.GetMemberCount()
-                    .ConfigureAwait(false);
-                var newMemberCount = newMembers.Length;
+                Log.Information("Welcome Message ignored because User is Global Banned..");
+                return;
+            }
 
-                Log.Information("Preparing send Welcome..");
+            var chatTitle = msg.Chat.Title;
+            var greet = Time.GetTimeGreet();
+            var memberCount = await _telegramService.GetMemberCount();
+            var newMemberCount = newMembers.Length;
 
-                if (chatSettings.WelcomeMessage.IsNullOrEmpty())
-                {
-                    chatSettings.WelcomeMessage = $"Hai {allNewMember}" +
-                                                  $"\nSelamat datang di kontrakan {chatTitle}" +
-                                                  $"\nKamu adalah anggota ke-{memberCount}";
-                }
+            Log.Information("Preparing send Welcome..");
 
-                var sendText = chatSettings.WelcomeMessage.ResolveVariable(new
-                {
-                    allNewMember,
-                    allNoUsername,
-                    allNewBot,
-                    chatTitle,
-                    greet,
-                    newMemberCount,
-                    memberCount
-                });
+            if (welcomeMessage.IsNullOrEmpty())
+            {
+                welcomeMessage = $"Hai {allNewMember}" +
+                                 $"\nSelamat datang di kontrakan {chatTitle}" +
+                                 $"\nKamu adalah anggota ke-{memberCount}";
+            }
 
-                InlineKeyboardMarkup keyboard = null;
-                if (!chatSettings.WelcomeButton.IsNullOrEmpty())
-                {
-                    keyboard = chatSettings.WelcomeButton.ToReplyMarkup(2);
-                }
+            var sendText = welcomeMessage.ResolveVariable(new
+            {
+                allNewMember,
+                allNoUsername,
+                allNewBot,
+                chatTitle,
+                greet,
+                newMemberCount,
+                memberCount
+            });
 
-                if (chatSettings.EnableHumanVerification)
-                {
-                    Log.Information("Human verification is enabled!");
-                    Log.Information("Adding verify button..");
+            InlineKeyboardMarkup keyboard = null;
+            if (!welcomeButton.IsNullOrEmpty())
+            {
+                keyboard = welcomeButton.ToReplyMarkup(2);
+            }
 
-                    var userId = newMembers[0].Id;
-                    var verifyButton = $"Saya Manusia!|verify {userId}";
+            if (Settings.EnableHumanVerification)
+            {
+                Log.Information("Human verification is enabled!");
+                Log.Information("Adding verify button..");
 
-                    var withVerifyArr = new string[] {chatSettings.WelcomeButton, verifyButton};
-                    var withVerify = string.Join(",", withVerifyArr);
+                var userId = newMembers[0].Id;
+                var verifyButton = $"Saya Manusia!|verify {userId}";
 
-                    keyboard = withVerify.ToReplyMarkup(2);
-                }
+                var withVerifyArr = new[] {welcomeButton, verifyButton};
+                var withVerify = string.Join(",", withVerifyArr);
 
-                var prevMsgId = chatSettings.LastWelcomeMessageId.ToInt();
+                keyboard = withVerify.ToReplyMarkup(2);
+            }
 
+            var prevMsgId = Settings.LastWelcomeMessageId.ToInt();
 
-                var sentMsgId = -1;
+            int sentMsgId;
 
-                if (chatSettings.WelcomeMediaType != MediaType.Unknown)
-                {
-                    var mediaType = (MediaType) chatSettings.WelcomeMediaType;
-                    sentMsgId = (await _telegramService.SendMediaAsync(
-                        chatSettings.WelcomeMedia,
-                        mediaType,
-                        sendText,
-                        keyboard).ConfigureAwait(false)).MessageId;
-                }
-                else
-                {
-                    sentMsgId = (await _telegramService.SendTextAsync(sendText, keyboard)
-                        .ConfigureAwait(false)).MessageId;
-                }
-
-                if (!chatSettings.EnableHumanVerification)
-                {
-                    await _telegramService.DeleteAsync(prevMsgId).ConfigureAwait(false);
-                }
-
-                await _settingsService.SaveSettingsAsync(new Dictionary<string, object>()
-                {
-                    {"chat_id", msg.Chat.Id},
-                    {"chat_title", msg.Chat.Title},
-                    {"chat_type", msg.Chat.Type},
-                    {"members_count", memberCount},
-                    {"last_welcome_message_id", sentMsgId}
-                }).ConfigureAwait(false);
-
-                await _settingsService.UpdateCache().ConfigureAwait(false);
+            Log.Debug("New Member handler before send. Time: {0}", stopwatch.Elapsed);
+            if (Settings.WelcomeMediaType != MediaType.Unknown)
+            {
+                var mediaType = Settings.WelcomeMediaType;
+                sentMsgId = (await _telegramService.SendMediaAsync(
+                    Settings.WelcomeMedia,
+                    mediaType,
+                    sendText,
+                    keyboard)).MessageId;
             }
             else
             {
-                Log.Information("Welcome Message ignored because User is Global Banned.");
+                sentMsgId = (await _telegramService.SendTextAsync(sendText, keyboard)).MessageId;
             }
+
+            if (!Settings.EnableHumanVerification)
+            {
+                await _telegramService.DeleteAsync(prevMsgId);
+            }
+
+            await _settingsService.SaveSettingsAsync(new Dictionary<string, object>()
+            {
+                {"chat_id", msg.Chat.Id},
+                {"chat_title", msg.Chat.Title},
+                {"chat_type", msg.Chat.Type},
+                {"members_count", memberCount},
+                {"last_welcome_message_id", sentMsgId}
+            });
+
+            // await _settingsService.UpdateCache();
+
+            Log.Debug("New Member handler complete. Time: {0}", stopwatch.Elapsed);
+            stopwatch.Stop();
         }
 
         private async Task<NewMember> ParseMemberCategory(User[] users)
@@ -171,30 +195,34 @@ namespace Zizi.Bot.Handlers.Events
             var allNoUsername = new StringBuilder();
             var allNewBot = new StringBuilder();
 
-            Log.Information($"Parsing new {users.Length} members..");
+            Log.Debug("Parsing new {0} members..", users.Length);
             foreach (var newMember in users)
             {
                 var newMemberId = newMember.Id;
 
+                var isGBanTask = _telegramService.CheckGlobalBanAsync(newMember);
+                var isCasBanTask = newMember.IsCasBanAsync();
+
                 if (Settings.EnableHumanVerification)
                 {
-                    Log.Information($"Restricting {newMemberId}");
-                    await _telegramService.RestrictMemberAsync(newMemberId)
-                        .ConfigureAwait(false);
+                    Log.Debug("Restricting {0}", newMemberId);
+                    await _telegramService.RestrictMemberAsync(newMemberId);
                 }
 
-                var isBan = await _telegramService.CheckGlobalBanAsync(newMember)
-                    .ConfigureAwait(false);
-                if (isBan) continue;
+                await Task.WhenAll(isCasBanTask, isGBanTask);
 
-                if (BotSettings.IsProduction)
-                {
-                    // var isCasBan = await IsCasBan(newMember.Id);
-                    await newMember.IsCasBanAsync().ConfigureAwait(false);
-                }
+                var isBan = await isGBanTask;
+                var isCasBan = await isCasBanTask;
+
+                if (isBan || isCasBan) continue;
+
+                // if (BotSettings.IsProduction)
+                // {
+                // var isCasBan = await IsCasBan(newMember.Id);
+                // }
 
                 var fullName = (newMember.FirstName + " " + newMember.LastName).Trim();
-                var nameLink = Members.GetNameLink(newMemberId, fullName);
+                var nameLink = newMemberId.GetNameLink(fullName);
 
                 if (newMember != lastMember)
                 {
