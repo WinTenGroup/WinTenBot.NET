dff23ae804fa4305f91ba1e326d7d40e1c59164f
Azhe-kun <azhe403@gmail.com>
2021-04-13 21:42:45 +0700

    fix rssinfo (get list rss in chat)

--- src/WinTenBot/Zizi.Bot/Handlers/Commands/Rss/RssInfoCommand.cs
+++ src/WinTenBot/Zizi.Bot/Handlers/Commands/Rss/RssInfoCommand.cs
@@ -1,54 +1,61 @@
 ﻿using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Framework.Abstractions;
 using Zizi.Bot.Models;
 using Zizi.Bot.Telegram;
 using Zizi.Bot.Services;
+using Zizi.Bot.Services.Datas;
+using Zizi.Core.Utils.Text;
 
 namespace Zizi.Bot.Handlers.Commands.Rss
 {
     public class RssInfoCommand : CommandBase
     {
-        private RssService _rssService;
-        private TelegramService _telegramService;
+        private readonly RssService _rssService;
+        private readonly TelegramService _telegramService;
+
+        public RssInfoCommand(RssService rssService, TelegramService telegramService)
+        {
+            _telegramService = telegramService;
+            _rssService = rssService;
+        }
 
         public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
             CancellationToken cancellationToken)
         {
-            _telegramService = new TelegramService(context);
-            _rssService = new RssService(context.Update.Message);
+            await _telegramService.AddUpdateContext(context);
 
             var chatId = _telegramService.Message.Chat.Id;
             var isAdmin = await _telegramService.IsAdminGroup();
 
-            if (isAdmin || _telegramService.IsPrivateChat())
+            if (!isAdmin && !_telegramService.IsPrivateChat)
             {
-                await _telegramService.SendTextAsync("🔄 Sedang meload data..");
-                var rssData = await _rssService.GetRssSettingsAsync(chatId);
-                var rssCount = rssData.Count();
-
-                var sendText = $"📚 <b>List RSS</b>: {rssCount} Items.";
-                int num = 1;
-                foreach (RssSetting rss in rssData)
-                {
-                    // var urlFeed = rss["url_feed"].ToString();
-                    sendText += $"\n{num++}. {rss.UrlFeed}";
-                }
-
-                if (rssCount == 0)
-                {
-                    sendText += "\n\nSepertinya kamu belum menambahkan RSS disini. " +
-                                "Kamu dapat menambahkan RSS dengan jumlah tidak terbatas!" +
-                                "\nGunakan <code>/setrss https://link_rss_nya</code> untuk menambahkan.";
-                }
-
-                await _telegramService.EditAsync(sendText);
+                await _telegramService.SendTextAsync("Kamu bukan admin, atau kamu bisa mengaturnya di japri.");
+                return;
             }
-            else
+
+            await _telegramService.SendTextAsync("🔄 Sedang meload data..");
+            var rssData = await _rssService.GetRssSettingsAsync(chatId);
+            var rssCount = rssData.Count;
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"📚 <b>List RSS</b>: {rssCount} Items.");
+            int num = 1;
+            foreach (var rss in rssData)
             {
-                await _telegramService.SendTextAsync("Kamu bukan admin, atau kamu bisa mengaturnya di japri ");
+                sb.Append($"{num++}. {rss.UrlFeed}");
             }
+
+            if (rssCount == 0)
+            {
+                sb.AppendLine("\nSepertinya kamu belum menambahkan RSS disini. ");
+                sb.AppendLine("Gunakan <code>/setrss https://link_rss_nya</code> untuk menambahkan.");
+            }
+            
+            await _telegramService.EditAsync(sb.ToTrimmedString());
         }
     }
 }
