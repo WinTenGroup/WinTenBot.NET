3fd291f84ab5a55d92b4ebc8a2fd309d554923cf
Azhe-kun <azhe403@gmail.com>
2021-04-14 23:15:56 +0700

    adding zippyshare.com for directlink generator

---
+++ src/WinTenBot/WinTenDev.Parser/Url/DirectLinkUtil.cs
@@ -0,0 +1,39 @@
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using Serilog;
+
+namespace WinTenDev.Parser.Url
+{
+    public class DirectLinkUtil
+    {
+        static readonly IConfiguration Config = Configuration.Default.WithDefaultLoader().WithJs().WithCss();
+
+        public static async Task<string> ParseZippyShare(string url)
+        {
+            var sw = Stopwatch.StartNew();
+
+            Log.Information("Preparing parse Zippyshare.com");
+            var context = BrowsingContext.New(Config);
+
+            Log.Debug("Loading web {Url}", url);
+            var document = await context.OpenAsync(url);
+
+            Log.Debug("Finding download button..");
+            var dl = document.QuerySelectorAll<IHtmlAnchorElement>("a")
+                .FirstOrDefault(x => x.Id == "dlbutton");
+
+            if (dl == null) return string.Empty;
+
+            Log.Debug("Getting direct link..");
+            var directLink = dl.Href;
+
+            Log.Information("Directlink of {Url} => {DirectLink}", url, directLink);
+
+            return directLink;
+        }
+    }
+}
