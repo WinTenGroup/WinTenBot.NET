4999392dd833fdda3bc7f86820cc7f68f51eca79
Azhe-kun <azhe403@gmail.com>
2021-04-15 17:23:29 +0700

    add learning model

---
+++ src/WinTenBot/Zizi.Bot/Models/MachineLearning/StopWord.cs
@@ -0,0 +1,12 @@
+namespace Zizi.Bot.Models.MachineLearning
+{
+    public class TextData
+    {
+        public string Text { get; set; }
+    }
+
+    public class TextTokens
+    {
+        public string[] Tokens { get; set; }
+    }
+}
