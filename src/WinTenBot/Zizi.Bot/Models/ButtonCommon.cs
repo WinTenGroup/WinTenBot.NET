c835ba499f251a1c6d66a4d2b9c5f4a5fd09dc09
Azhe-kun <azhe403@gmail.com>
2021-04-16 21:41:23 +0700

    add button markup

---
+++ src/WinTenBot/Zizi.Bot/Models/ButtonCommon.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Zizi.Bot.Models
+{
+    public class ButtonMarkup
+    {
+        [JsonProperty("text")]
+        public string Text { get; set; }
+        
+        [JsonProperty("data")]
+        public string Data { get; set; }
+    }
+}
