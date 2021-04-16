afe6cd3d063d9f6bcca256582e1a0ff3cc984949
Azhe-kun <azhe403@gmail.com>
2021-04-16 20:28:29 +0700

    fix new Update handler

--- src/WinTenBot/Zizi.Bot/Handlers/NewUpdateHandler.cs
+++ src/WinTenBot/Zizi.Bot/Handlers/NewUpdateHandler.cs
@@ -1,75 +1,90 @@
-﻿using Serilog;
-using SqlKata.Execution;
+using Humanizer;
+using Serilog;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Framework.Abstractions;
 using Zizi.Bot.Common;
+using Zizi.Bot.Models;
 using Zizi.Bot.Models.Settings;
 using Zizi.Bot.Services;
+using Zizi.Bot.Services.Datas;
+using Zizi.Bot.Services.Features;
 using Zizi.Bot.Telegram;
 
 namespace Zizi.Bot.Handlers
 {
     public class NewUpdateHandler : IUpdateHandler
     {
-        private TelegramService _telegramService;
-        private AppConfig AppConfig { get; set; }
-        private QueryFactory QueryFactory { get; set; }
+        private readonly TelegramService _telegramService;
+        private readonly AfkService _afkService;
+        private readonly AntiSpamService _antiSpamService;
+        private readonly SettingsService _settingsService;
 
-        public NewUpdateHandler(AppConfig appConfig, QueryFactory queryFactory)
+        private ChatSetting _chatSettings;
+        private readonly AppConfig _appConfig;
+
+        public NewUpdateHandler(
+            AfkService afkService,
+            AppConfig appConfig,
+            AntiSpamService antiSpamService,
+            SettingsService settingsService,
+            TelegramService telegramService
+        )
         {
-            AppConfig = appConfig;
-            QueryFactory = queryFactory;
+            _appConfig = appConfig;
+            _afkService = afkService;
+            _antiSpamService = antiSpamService;
+            _telegramService = telegramService;
+            _settingsService = settingsService;
         }
 
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
-            _telegramService = new TelegramService(context);
+            await _telegramService.AddUpdateContext(context);
+
             if (_telegramService.Context.Update.ChannelPost != null) return;
 
-            _telegramService.AppConfig = AppConfig;
-            // _telegramService.IsBotAdmin = await _telegramService.IsBotAdmin().ConfigureAwait(false);
-            // _telegramService.IsFromAdmin = await _telegramService.IsAdminChat().ConfigureAwait(false);
+            _telegramService.IsTooOld();
 
-            var chatSettings = _telegramService.CurrentSetting;
+            _chatSettings = _telegramService.CurrentSetting;
 
-            Log.Debug("NewUpdate MessageType: {0}", _telegramService.AnyMessage.Type);
             Log.Debug("NewUpdate: {0}", _telegramService.Context.Update.ToJson(true));
 
             // Pre-Task is should be awaited.
-            await EnqueuePreTask().ConfigureAwait(false);
+            await EnqueuePreTask();
 
-            if (chatSettings.EnableWarnUsername && _telegramService.IsGroupChat())
+            if (_chatSettings.EnableWarnUsername
+                && _telegramService.IsGroupChat())
             {
                 Log.Debug("Await next condition 1. is enable Warn Username && is Group Chat.");
                 if (!_telegramService.IsNoUsername || _telegramService.MessageOrEdited.Text == null)
                 {
                     // Next, do what bot should do.
                     Log.Debug("Await next condition on sub condition 1. is has Username || MessageText == null");
-                    await next(context, cancellationToken).ConfigureAwait(false);
+                    await next(context, cancellationToken);
                 }
             }
-            else if (_telegramService.IsPrivateChat())
+            else if (_telegramService.IsPrivateChat)
             {
                 Log.Debug("Await next condition 2. if private chat");
-                await next(context, cancellationToken).ConfigureAwait(false);
+                await next(context, cancellationToken);
             }
             else
             {
                 Log.Debug("Await next else condition.");
-                await next(context, cancellationToken).ConfigureAwait(false);
+                await next(context, cancellationToken);
             }
 
             //
             // if (_telegramService.MessageOrEdited.Text == null)
             // {
-            //     await next(context, cancellationToken).ConfigureAwait(false);
+            //     await next(context, cancellationToken);
             // }
 
             // Last, do additional task which bot may do
-            EnqueueBackgroundTask();
+            await EnqueueBackgroundTask();
         }
 
         private async Task EnqueuePreTask()
@@ -77,17 +92,16 @@ namespace Zizi.Bot.Handlers
             Log.Information("Enqueue pre tasks");
             var sw = Stopwatch.StartNew();
 
-            var message = _telegramService.MessageOrEdited;
+            var message = _telegramService.AnyMessage;
             var callbackQuery = _telegramService.CallbackQuery;
 
-            // var actions = new List<Action>();
             var shouldAwaitTasks = new List<Task>();
 
-            if (!_telegramService.IsPrivateChat())
+            if (!_telegramService.IsPrivateChat)
             {
                 shouldAwaitTasks.Add(_telegramService.EnsureChatRestrictionAsync());
 
-                if (message.Text != null)
+                if (message?.Text != null)
                 {
                     shouldAwaitTasks.Add(_telegramService.CheckGlobalBanAsync());
                     shouldAwaitTasks.Add(_telegramService.CheckCasBanAsync());
@@ -103,15 +117,13 @@ namespace Zizi.Bot.Handlers
 
             Log.Debug("Awaiting should await task..");
 
-            await Task.WhenAll(shouldAwaitTasks.ToArray())
-                .ConfigureAwait(false);
+            await Task.WhenAll(shouldAwaitTasks.ToArray());
 
             Log.Debug("All preTask completed in {0}", sw.Elapsed);
             sw.Stop();
         }
 
-
-        private void EnqueueBackgroundTask()
+        private async Task EnqueueBackgroundTask()
         {
             var nonAwaitTasks = new List<Task>();
             var message = _telegramService.MessageOrEdited;
@@ -130,10 +142,9 @@ namespace Zizi.Bot.Handlers
             nonAwaitTasks.Add(_telegramService.CheckMataZiziAsync());
             nonAwaitTasks.Add(_telegramService.HitActivityAsync());
 
-#pragma warning disable 4014
             // This List Task should not await.
-            Task.WhenAny(nonAwaitTasks.ToArray());
-#pragma warning restore 4014
+            await Task.WhenAll(nonAwaitTasks.ToArray());
+        }
         }
     }
 }
