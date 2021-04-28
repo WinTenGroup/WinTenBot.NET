b0731cfbc7a3acd95e3e7602a90ca404ed9fd077
Azhe-kun <azhe403@gmail.com>
2021-04-28 11:48:01 +0700

    improve weather reporter handler

--- src/WinTenBot/Zizi.Bot/Handlers/WeatherReporter.cs
+++ src/WinTenBot/Zizi.Bot/Handlers/WeatherReporter.cs
@@ -1,37 +1,39 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot.Framework.Abstractions;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
 using Zizi.Bot.Interfaces;
+using Zizi.Bot.Services.Features;
 
 namespace Zizi.Bot.Handlers
 {
     class WeatherReporter : IUpdateHandler
     {
         private readonly IWeatherService _weatherService;
+        private readonly TelegramService _telegramService;
 
-        public WeatherReporter(IWeatherService weatherService)
+        public WeatherReporter(
+            IWeatherService weatherService,
+            TelegramService telegramService
+        )
         {
+            _telegramService = telegramService;
             _weatherService = weatherService;
         }
 
         public async Task HandleAsync(IUpdateContext context, UpdateDelegate next, CancellationToken cancellationToken)
         {
-            Message msg = context.Update.Message;
-            Location location = msg.Location;
+            await _telegramService.AddUpdateContext(context);
+
+            var location = _telegramService.Message.Location;
 
             var weather = await _weatherService.GetWeatherAsync(location.Latitude, location.Longitude);
 
-            await context.Bot.Client.SendTextMessageAsync(
-                msg.Chat,
-                $"Weather status is *{weather.Status}* with the temperature of {weather.Temp:F1}.\n" +
-                $"Min: {weather.MinTemp:F1}\n" +
-                $"Max: {weather.MaxTemp:F1}\n\n\n" +
-                "powered by [MetaWeather](https://www.metaweather.com)",
-                ParseMode.Markdown,
-                replyToMessageId: msg.MessageId
-            );
+            var text = $"Weather status is <b>{weather.Status}</b> with the temperature of {weather.Temp:F1}.\n" +
+                       $"Min: {weather.MinTemp:F1}\n" +
+                       $"Max: {weather.MaxTemp:F1}\n\n" +
+                       "powered by <a href='https://www.metaweather.com'>MetaWeather</a>";
+
+            await _telegramService.SendTextAsync(text);
         }
     }
 }
