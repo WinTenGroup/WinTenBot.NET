861112bcd715c15bd48179a9c755741d7bc3c9f9
Azhe-kun <azhe403@gmail.com>
2021-05-24 20:25:12 +0700

    fix log uploader

--- src/WinTenBot/WinTenDev.Zizi.Services/StorageService.cs
+++ src/WinTenBot/WinTenDev.Zizi.Services/StorageService.cs
@@ -2,7 +2,9 @@
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 using Serilog;
+using Telegram.Bot;
 using Telegram.Bot.Types.InputFiles;
 using WinTenDev.Zizi.Models.Configs;
 using WinTenDev.Zizi.Utils.IO;
@@ -11,13 +13,24 @@ namespace WinTenDev.Zizi.Services
 {
     public class StorageService
     {
+        private CommonConfig _commonConfig;
+        private readonly TelegramBotClient _botClient;
+
+        public StorageService(
+            IOptionsSnapshot<CommonConfig> optionsCommonConfig,
+            TelegramBotClient botClient
+        )
+        {
+            _commonConfig = optionsCommonConfig.Value;
+            _botClient = botClient;
+        }
+
         public async Task ClearLog()
         {
             try
             {
                 const string logsPath = "Storage/Logs";
-                var botClient = BotSettings.Client;
-                var channelTarget = BotSettings.BotChannelLogs;
+                var channelTarget = _commonConfig.ChannelLogs;
 
                 if (!channelTarget.ToString().StartsWith("-100"))
                 {
@@ -27,15 +40,11 @@ namespace WinTenDev.Zizi.Services
 
                 var dirInfo = new DirectoryInfo(logsPath);
                 var files = dirInfo.GetFiles();
-                // var filteredCreated = files.Where(fileInfo => fileInfo.LastWriteTimeUtc < DateTime.UtcNow.AddDays(-1)).ToList();
-                // var filteredModified = files.Where(fileInfo => fileInfo.CreationTimeUtc < DateTime.UtcNow.AddDays(-1)).ToList();
 
                 var filteredFile = files.Where(fileInfo =>
                     fileInfo.LastWriteTimeUtc < DateTime.UtcNow.AddDays(-1)
                     || fileInfo.CreationTimeUtc < DateTime.UtcNow.AddDays(-1)
                 ).ToList();
-
-                // filteredCreated.AddRange(filteredModified);
 
                 var fileCount = filteredFile.Count;
 
@@ -52,13 +61,15 @@ namespace WinTenDev.Zizi.Services
                         var media = new InputOnlineFile(fileStream, zipFile);
                         media.FileName = Path.GetFileName(zipFile);
 
-                        await botClient.SendDocumentAsync(channelTarget, media);
+                        await _botClient.SendDocumentAsync(channelTarget, media);
 
                         fileStream.Close();
                         await fileStream.DisposeAsync();
 
                         filePath.DeleteFile();
                         zipFile.DeleteFile();
+
+                        Log.Information("Upload file {ZipFile} succeeded", zipFile);
                     }
                 }
                 else
@@ -68,65 +79,7 @@ namespace WinTenDev.Zizi.Services
             }
             catch (Exception ex)
             {
-                Log.Error(ex, "Error Send .Log file to ChannelTarget.");
-            }
-        }
-
-        public async Task ClearLogs(string logsPath, string filter = "", bool upload = true)
-        {
-            try
-            {
-                Log.Information("Clearing {LogsPath}, filter: {Filter}, upload: {Upload}", logsPath, filter, upload);
-                var botClient = BotSettings.Client;
-                var channelTarget = BotSettings.BotChannelLogs;
-
-                if (!channelTarget.ToString().StartsWith("-100"))
-                {
-                    Log.Information("Please specify ChannelTarget in appsettings.json");
-                    return;
-                }
-
-                var dirInfo = new DirectoryInfo(logsPath);
-                var files = dirInfo.GetFiles();
-                var filteredFiles = files.Where(fileInfo =>
-                    fileInfo.CreationTimeUtc < DateTime.UtcNow.AddDays(-1) &&
-                    fileInfo.FullName.Contains(filter)).ToList();
-
-                // filteredCreated.AddRange(filteredModified);
-
-                var filteredFilesCount = filteredFiles.Count;
-
-                if (filteredFilesCount > 0)
-                {
-                    Log.Information("Found {Length} of {Length1}", filteredFilesCount, files.Length);
-                    foreach (var fileInfo in filteredFiles)
-                    {
-                        var filePath = fileInfo.FullName;
-                        if (upload)
-                        {
-                            Log.Information("Uploading file {FilePath}", filePath);
-                            var fileStream = File.OpenRead(filePath);
-
-                            var media = new InputOnlineFile(fileStream, fileInfo.Name);
-                            await botClient.SendDocumentAsync(channelTarget, media);
-
-                            fileStream.Close();
-                            await fileStream.DisposeAsync();
-                        }
-
-                        var old = filePath + ".old";
-                        File.Move(filePath, old);
-                        old.DeleteFile();
-                    }
-                }
-                else
-                {
-                    Log.Information("No Logs file need be processed for previous date");
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.Error(ex, "Error Send .Log file to ChannelTarget.");
+                Log.Error(ex, "Error Send .Log file to ChannelTarget");
             }
         }
     }
