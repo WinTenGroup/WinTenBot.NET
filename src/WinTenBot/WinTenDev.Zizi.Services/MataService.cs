4f748940a735525d655648fa021a5afcac837b53
Azhe-kun <azhe403@gmail.com>
2021-05-24 20:44:42 +0700

    fix mata service

--- src/WinTenBot/WinTenDev.Zizi.Services/MataService.cs
+++ src/WinTenBot/WinTenDev.Zizi.Services/MataService.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using EasyCaching.Core;
+using Serilog;
 using WinTenDev.Zizi.Models.Types;
 using WinTenDev.Zizi.Utils;
 
 namespace WinTenDev.Zizi.Services
 {
+    /// <summary>
+    /// Track user property changes
+    /// </summary>
     public class MataService
     {
         private string baseKey = "zizi-mata";
         private readonly IEasyCachingProvider _cachingProvider;
 
+        /// <summary>
+        /// Mata constructor
+        /// </summary>
+        /// <param name="cachingProvider"></param>
         public MataService
         (
             IEasyCachingProvider cachingProvider
@@ -19,17 +26,35 @@ namespace WinTenDev.Zizi.Services
             _cachingProvider = cachingProvider;
         }
 
+        private string GetCacheKey(int fromId)
+        {
+            return $"{baseKey}_{fromId}";
+        }
+
+
+        /// <summary>
+        /// Get Mata by userId
+        /// </summary>
+        /// <param name="fromId"></param>
+        /// <returns></returns>
         public async Task<CacheValue<HitActivity>> GetMataCore(int fromId)
         {
-            var key = baseKey + fromId;
+            var key = GetCacheKey(fromId);
             var hitActivity = await _cachingProvider.GetAsync<HitActivity>(key);
             return hitActivity;
         }
 
+        /// <summary>
+        /// Save Mata by userId
+        /// </summary>
+        /// <param name="fromId"></param>
+        /// <param name="hitActivity"></param>
         public async Task SaveMataAsync(int fromId, HitActivity hitActivity)
         {
-            var key = baseKey + fromId;
-            await _cachingProvider.SetAsync(key, hitActivity, TimeUtil.YearSpan(30));
+            var key = GetCacheKey(fromId);
+            var timeSpan = TimeUtil.YearSpan(30);
+            Log.Debug("Saving Mata into cache with Key: '{Key}'. Span: {TimeSpan}", key, timeSpan);
+            await _cachingProvider.SetAsync(key, hitActivity, timeSpan);
         }
     }
 }
