af5e51d29580776e918100701edb8b468d5dac9c
Azhe-kun <azhe403@gmail.com>
2021-05-24 20:50:03 +0700

    register one cron per admin checker

--- src/WinTenBot/WinTenDev.Zizi.Services/ChatService.cs
+++ src/WinTenBot/WinTenDev.Zizi.Services/ChatService.cs
@@ -5,6 +5,7 @@ using Hangfire;
 using Serilog;
 using SqlKata.Execution;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using WinTenDev.Zizi.Utils;
@@ -14,7 +15,9 @@ namespace WinTenDev.Zizi.Services
 {
     public class ChatService
     {
-        private string _adminCheckerPrefix = "admin-checker";
+        private const string AdminCheckerPrefix = "admin-checker";
+        private const int PrivateSettingLimit = 60;
+        private const int AfterLeaveLimit = 30;
 
         private readonly QueryFactory _queryFactory;
         private readonly TelegramBotClient _botClient;
@@ -41,14 +44,27 @@ namespace WinTenDev.Zizi.Services
             // var urlAddTo = await _botClient.GetUrlStart("startgroup=new");
 
             var allSettings = _settingsService.GetAllSettings();
-            foreach (var chatGroup in allSettings)
+            foreach (var chatSetting in allSettings)
             {
-                var chatId = chatGroup.ChatId.ToInt64();
+                var chatId = chatSetting.ChatId.ToInt64();
                 var reducedChatId = chatId.ReduceChatId();
 
-                var adminCheckerId = _adminCheckerPrefix + "-" + reducedChatId;
+                var adminCheckerId = AdminCheckerPrefix + "-" + reducedChatId;
 
-                HangfireUtil.RegisterJob<ChatService>(adminCheckerId, (ChatService service) => service.AdminCheckerJobAsync(chatId), Cron.Daily, queue: "admin-checker");
+                if (chatSetting.ChatType != ChatType.Private)
+                {
+                    Log.Debug("Creating Chat Jobs for ChatID '{ChatId}'", chatId);
+                    HangfireUtil.RegisterJob<ChatService>(adminCheckerId, (ChatService service) => service.AdminCheckerJobAsync(chatId), Cron.Daily, queue: "admin-checker");
+                }
+                else
+                {
+                    var dateNow = DateTime.UtcNow;
+                    if ((dateNow - chatSetting.UpdatedAt).TotalDays > PrivateSettingLimit)
+                    {
+                        Log.Debug("This time is cleanup this chat!");
+                        await _settingsService.DeleteSettings(chatId);
+                    }
+                }
             }
         }
 
@@ -101,35 +117,58 @@ namespace WinTenDev.Zizi.Services
             }
         }
 
-        [AutomaticRetry(Attempts = 2, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
+        [AutomaticRetry(Attempts = 1, OnAttemptsExceeded = AttemptsExceededAction.Delete)]
         public async Task AdminCheckerJobAsync(long chatId)
         {
-            var sw = Stopwatch.StartNew();
-            Log.Information("Starting check Admin in ChatID '{ChatId}'", chatId);
-
-            var me = await _botClient.GetMeAsync();
-            var isAdminChat = await _botClient.IsAdminChat(chatId, me.Id);
-
-            if (isAdminChat) return;
-
-            Log.Debug("Doing leave chat from {ChatId}", chatId);
-            var msgLeave = "Sepertinya saya bukan admin di grup ini, saya akan meninggalkan grup. Sampai jumpa!" +
-                           "\n\nTerima kasih sudah menggunakan @MissZiziBot, silakan undang saya kembali jika diperlukan.";
-
-            var inlineKeyboard = new InlineKeyboardMarkup(new[]
+            try
             {
-                new[]
+                var sw = Stopwatch.StartNew();
+                Log.Information("Starting check Admin in ChatID '{ChatId}'", chatId);
+
+                var me = await _botClient.GetMeAsync();
+                var isAdminChat = await _botClient.IsAdminChat(chatId, me.Id);
+
+                if (isAdminChat) return;
+
+                Log.Debug("Doing leave chat from {ChatId}", chatId);
+                var msgLeave = "Sepertinya saya bukan admin di grup ini, saya akan meninggalkan grup. Sampai jumpa!" +
+                               "\n\nTerima kasih sudah menggunakan @MissZiziBot, silakan undang saya kembali jika diperlukan.";
+
+                var inlineKeyboard = new InlineKeyboardMarkup(new[]
                 {
-                    InlineKeyboardButton.WithUrl("👥 Dukungan Grup", "https://t.me/WinTenDev")
-                    // InlineKeyboardButton.WithUrl("↖️ Tambahkan ke Grup", urlAddTo)
+                    new[]
+                    {
+                        InlineKeyboardButton.WithUrl("👥 Dukungan Grup", "https://t.me/WinTenDev")
+                        // InlineKeyboardButton.WithUrl("↖️ Tambahkan ke Grup", urlAddTo)
+                    }
+                });
+
+                await _botClient.SendTextMessageAsync(chatId, msgLeave, ParseMode.Html, replyMarkup: inlineKeyboard);
+                await _botClient.LeaveChatAsync(chatId);
+
+                Log.Debug("Checking Admin in ChatID '{ChatId}' job complete in {Elapsed}", chatId, sw.Elapsed);
+                sw.Stop();
+            }
+            catch (ApiRequestException requestException)
+            {
+                Log.Error(requestException, "Error API Request when Check Admin in ChatID: '{ChatId}'", chatId);
+                var setting = await _settingsService.GetSettingsByGroupCore(chatId);
+
+                var exMessage = requestException.Message.ToLower();
+                if (exMessage.IsContains("forbidden"))
+                {
+                    if ((DateTime.UtcNow - setting.UpdatedAt).TotalDays > AfterLeaveLimit)
+                    {
+                        Log.Debug("This time is cleanup this chat!");
+                        await _settingsService.DeleteSettings(chatId);
+                    }
                 }
-            });
-
-            await _botClient.SendTextMessageAsync(chatId, msgLeave, ParseMode.Html, replyMarkup: inlineKeyboard);
-            await _botClient.LeaveChatAsync(chatId);
-
-            Log.Debug("Checking Admin in ChatID '{ChatId}' job complete in {Elapsed}", chatId, sw.Elapsed);
-            sw.Stop();
+            }
+            catch (Exception e)
+            {
+                Log.Error(e, "Error when Check Admin in ChatID: '{ChatId}'", chatId);
+                throw;
+            }
         }
     }
 }
