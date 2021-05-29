07af7869e0f357fd01058a83ad3e03e47bd1aa61
Azhe-kun <azhe403@gmail.com>
2021-05-29 07:05:21 +0700

    add Func util

---
+++ src/WinTenBot/WinTenDev.Zizi.Utils/FuncUtil.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace WinTenDev.Zizi.Utils
+{
+    /// <summary>
+    /// Extension method about Func
+    /// </summary>
+    public static class FuncUtil
+    {
+        /// <summary>
+        /// Run a function and ignore
+        /// </summary>
+        /// <param name="task"></param>
+        public static void FireAndForget(this Task task)
+        {
+            task.Ignore();
+        }
+    }
+}
