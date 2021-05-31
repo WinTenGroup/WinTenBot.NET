75ad16400095798e4048fd805a2086f90c12e686
Azhe-kun <azhe403@gmail.com>
2021-05-31 16:40:35 +0700

    moving custom bot option to models

--- src/WinTenBot/WinTenDev.Zizi.Models/Bots/Options/CustomBotOptions.cs
+++ src/WinTenBot/WinTenDev.Zizi.Models/Bots/Options/CustomBotOptions.cs
@@ -1,7 +1,7 @@
 ﻿using Telegram.Bot.Framework;
 using Telegram.Bot.Framework.Abstractions;
 
-namespace WinTenDev.Zizi.Host.Options
+namespace WinTenDev.Zizi.Models.Bots.Options
 {
     public class CustomBotOptions<TBot> : BotOptions<TBot>
         where TBot : IBot
