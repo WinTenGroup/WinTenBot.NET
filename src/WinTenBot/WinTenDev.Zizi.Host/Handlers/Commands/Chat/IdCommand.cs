f4eccc5074c7c8da2fa298c179b4b2d34a7857db
Azhe-kun <azhe403@gmail.com>
2021-05-31 11:13:14 +0700

    include reply member id on reply

--- src/WinTenBot/WinTenDev.Zizi.Host/Handlers/Commands/Chat/IdCommand.cs
+++ src/WinTenBot/WinTenDev.Zizi.Host/Handlers/Commands/Chat/IdCommand.cs
@@ -3,29 +3,39 @@ using System.Threading.Tasks;
 using Telegram.Bot.Framework.Abstractions;
 using WinTenDev.Zizi.Services;
 using WinTenDev.Zizi.Utils;
+using WinTenDev.Zizi.Utils.Telegram;
 
 namespace WinTenDev.Zizi.Host.Handlers.Commands.Chat
 {
+    /// <summary>
+    /// Get simple identity of Chat and User
+    /// </summary>
     public class IdCommand : CommandBase
     {
         private readonly TelegramService _telegramService;
 
+        /// <summary>
+        /// ID Constructor
+        /// </summary>
+        /// <param name="telegramService"></param>
         public IdCommand(TelegramService telegramService)
         {
             _telegramService = telegramService;
         }
 
+        /// <summary>
+        /// Handle get ID command
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="next"></param>
+        /// <param name="args"></param>
+        /// <param name="cancellationToken"></param>
         public override async Task HandleAsync(IUpdateContext context, UpdateDelegate next, string[] args,
             CancellationToken cancellationToken)
         {
             await _telegramService.AddUpdateContext(context);
 
-            var msg = context.Update.Message;
-
-            if (msg.ReplyToMessage != null)
-            {
-                msg = msg.ReplyToMessage;
-            }
+            var msg = _telegramService.Message;
 
             var chatTitle = msg.Chat.Title;
             var chatId = msg.Chat.Id;
@@ -33,7 +43,7 @@ namespace WinTenDev.Zizi.Host.Handlers.Commands.Chat
 
             var userId = msg.From.Id;
             var username = msg.From.Username;
-            var name = (msg.From.FirstName + " " + msg.From.LastName).Trim();
+            var name = msg.From.GetFullName();
             var userLang = msg.From.LanguageCode;
 
             var text = $"👥 <b>{chatTitle}</b>\n" +
@@ -43,6 +53,17 @@ namespace WinTenDev.Zizi.Host.Handlers.Commands.Chat
                        $"ID: <code>{userId}</code>\n" +
                        $"Username: @{username}\n" +
                        $"Language: {userLang.ToUpperCase()}";
+
+            if (msg.ReplyToMessage != null)
+            {
+                var repMsg = msg.ReplyToMessage;
+                var repFullName = repMsg.From.GetFullName();
+
+                text += $"\n\n👤 <b>{repFullName}</b>" +
+                        $"\nID: <code>{repMsg.From.Id}</code>" +
+                        $"\nUsername: @{repMsg.From.Username}" +
+                        $"\nLanguage: {repMsg.From.LanguageCode}";
+            }
 
             await _telegramService.SendTextAsync(text);
         }
