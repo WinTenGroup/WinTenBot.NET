75672ca6c38654e9dba0be8187309e90b714832a
Azhe-kun <azhe403@gmail.com>
2021-06-21 18:20:12 +0700

    add original Telegram.Bot.Framework project with some fixes

---
+++ src/WinTenBot/Telegram.Bot.Framework/Abstractions/IBotBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Telegram.Bot.Framework.Abstractions
+{
+    public interface IBotBuilder
+    {
+        IBotBuilder Use(Func<UpdateDelegate, UpdateDelegate> middleware);
+
+        IBotBuilder Use<THandler>()
+            where THandler : IUpdateHandler;
+
+        IBotBuilder Use<THandler>(THandler handler)
+            where THandler : IUpdateHandler;
+
+        UpdateDelegate Build();
+    }
+}
